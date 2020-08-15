c22a543d354f153911d6e6e32bc955383ac083c3
Aunyamanee Techa <auntecha@gmail.com>
2020-08-15 15:12:13 +0700

    เข้าใจ rontroller และ view นิดหน่อย

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "HomeController@index");
+
+Route::get('/about', "HomeController@about");
+
+Route::get('/contact', "HomeController@contact");
+
+Route::get('/user', "HomeController@index");
+
+Route::get('/user/create', "HomeController@create");
+
+Route::get('/user/{name}', "HomeController@showName");
+
+Route::get('/user/{name}/{age}', "HomeController@showNameAndAge");
